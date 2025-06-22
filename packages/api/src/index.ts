9c3ee9cfcf7c262a84f7e545a17b13e96197f26e
mattcattb <mboughton17@gmail.com>
2025-06-22 19:22:23 +0000

    finished basic API stuff!

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,6 +1,11 @@
+import {createChildLogger} from "@/common/logger";
 import {app} from "./app";
+const startupLogger = createChildLogger("index");
+
+const port = parseInt(process.env.PORT || "3000");
+startupLogger.info(`API server attempting to listen on port ${port}`);
 
 export default {
   fetch: app.fetch,
-  hostname: process.env.HOSTNAME,
+  port: port,
 };
